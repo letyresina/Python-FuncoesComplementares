1a67e1410e2d7bed0e2d5793a32c14c79cd081a4
letyresina <leticiaresina@gmail.com>
2023-05-05 11:19:58 -0300

    Colocando site pra facilitar a construção do exercício 2

--- exercicio2.py
+++ exercicio2.py
@@ -4,3 +4,6 @@
     2730 é triangular, pois 13 * 14 * 15 = 2730. Faça uma função que receba um número inteiro e 
     retorne True se for um número triangular e False, caso contrário.
 '''
+
+# ler esse site https://mundoeducacao.uol.com.br/matematica/numeros-triangulares.htm#:~:text=Portanto%2C%20os%20n%C3%BAmeros%20triangulares%20podem,1%20e%20primeiro%20termo%201. para fazer
+
