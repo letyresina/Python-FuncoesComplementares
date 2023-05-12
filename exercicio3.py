e6a7626ef45bdc4f44def3b85fc0b79251dff624
letyresina <leticiaresina@gmail.com>
2023-05-12 11:45:56 -0300

    Iniciado exercicio 3, terminar em casa

--- exercicio3.py
+++ exercicio3.py
@@ -2,8 +2,13 @@
     Em um jogo de dados, pode ser sorteado qualquer número entre 1 e 6. Faça uma função que simule
     1 milhão de lançamentos de dados e mostre quantas vezes cada número foi sorteado.
 '''
+import random;
 
 def sorteio():
-    for i in range(1, 1001):
-        
+    for a, b, c, d, e, f in range(1, 1001):
+        num = random.randint(1,6)
         i += 1
+        print(num)
+
+sorteio()
+
