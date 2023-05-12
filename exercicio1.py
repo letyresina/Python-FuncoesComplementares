8122568c211800cd91a01f0d02213aaef3b2fb79
letyresina <leticiaresina@gmail.com>
2023-05-12 10:49:58 -0300

    Pequena correção no exercicio 1

--- exercicio1.py
+++ exercicio1.py
@@ -4,7 +4,7 @@
 '''
 
 def tresNumeros(num1, num2, num3):
-    if (num1 + num2 == num3):
+    if ((num1 + num2 == num3) or (num1 + num3 == num2) or (num2 + num3 == num1)):
         return True
     else:
         return False
@@ -19,7 +19,6 @@ try:
         print(f"A soma entre {num1} e {num2} é igual à {num3}")
     else:
         print(f"A soma entre {num1} + {num2} não é igual à {num3}")
-        
 except ValueError:
     print(f"Por favor, digite um valor válido!")
 
